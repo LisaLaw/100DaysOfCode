62bee6c78a13ee14e594fc6f3ded7f68f0c57c45
LisaLaw <62116039+LisaLaw@users.noreply.github.com>
2020-06-01 21:17:02 +0100

    Create password_validator_spanish.py

---
+++ Python/password_validator_spanish.py
@@ -0,0 +1,13 @@
+pw = input('Elige una contraseña:')
+
+def validate(password):
+    if len(pw) < 6:
+        print('La contraseña debe tener 6 caracteres como mínimo.')  
+    elif len(pw) > 12:
+        print('La contraseña no debe tener más de 12 caracteres.')
+    elif not pw.isalnum:
+        print('La contraseña debe ser alfanumérica.')
+    else:
+        print('Tu contraseña se ha guardado.')
+
+validate(pw)
