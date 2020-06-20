eb77b6c66ba8e5f982949421c15dce30f7f03dca
LisaLaw <62116039+LisaLaw@users.noreply.github.com>
2020-06-20 14:31:47 +0100

    tidied up, fixed errors

--- 8 - Burned Calories Calculator/burned calories.js
+++ 8 - Burned Calories Calculator/burned calories.js
@@ -1,4 +1,25 @@
-const form = document.querySelector("form")
+document.addEventListener('DOMContentLoaded', function() {
+    getSum();
+})
+let sum = 0
+function getSum() {
+    document.addEventListener('click', function() {
+        let exercises = document.querySelectorAll('li');
+        exercises.forEach(function(exercise) {
+            sum = sum + 1;
+            console.log(sum)
+        });
+    });
+    
+
+}
+
+
+
+
+
+
+/*const form = document.querySelector("form")
 
 const keys = form.querySelector(".switch")
 
@@ -8,4 +29,4 @@ keys.addEventListener("click", e => {
     }
 })
 
-const key = e.target
+const key = e.target*/
