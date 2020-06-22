344aa1d75280c8d1127e4558af35761c9b02ea43
LisaLaw <62116039+LisaLaw@users.noreply.github.com>
2020-06-22 19:34:58 +0100

    all if conditions included

--- 13_dropdown_navbar/navbar.js
+++ 13_dropdown_navbar/navbar.js
@@ -14,12 +14,30 @@ let secondDropdownBtn = document.getElementsByClassName('dropdown')[1];
 let firstInnerLinks = document.getElementsByClassName('inner-list')[0];
 let secondInnerLinks = document.getElementsByClassName('inner-list')[1];
 
-firstDropdownBtn.addEventListener('click', () => {
-    firstInnerLinks.classList.toggle('active-dropdown');
-    secondInnerLinks.classList.toggle('inactive-dropdown');
+//shop+ btn:
+firstDropdownBtn.addEventListener('click', () => { //on click, do the following
+    if (secondInnerLinks.classList == 'inactive-dropdown' && firstInnerLinks.classList == 'inactive-dropdown') { //if both buttons are closed
+        firstInnerLinks.classList.remove('inactive-dropdown'); //remove closure from this btn
+        firstInnerLinks.classList.add('active-dropdown'); // add opening to this btn
+    } else if (secondInnerLinks.classList == 'inactive-dropdown' && firstInnerLinks.classList == 'active-dropdown') { //if other btn is closed and this btn is open
+        firstInnerLinks.classList.remove('active-dropdown'); //remove opening from this btn
+        firstInnerLinks.classList.add('inactive-dropdown'); //add closure to this btn
+    } else { //else the other btn is open
+        secondInnerLinks.classList.remove('active-dropdown'); //remove opening from other btn
+        firstInnerLinks.classList.toggle('active-dropdown'); //toggle open-close to this btn
+    }
 })
 
-secondDropdownBtn.addEventListener('click', () => {
-    secondInnerLinks.classList.toggle('active-dropdown');
-    firstInnerLinks.classList.toggle('inactive-dropdown');
+//vivo+ btn
+secondDropdownBtn.addEventListener('click', () => { //on click, do the following
+    if (firstInnerLinks.classList == 'inactive-dropdown' && secondInnerLinks.classList == 'inactive-dropdown') { //if both buttons are closed
+        secondInnerLinks.classList.remove('inactive-dropdown'); //remove closure from this btn
+        secondInnerLinks.classList.add('active-dropdown'); // add opening to this btn
+    } else if (firstInnerLinks.classList == 'inactive-dropdown' && secondInnerLinks.classList == 'active-dropdown') { //if other btn is closed and this btn is open
+        secondInnerLinks.classList.remove('active-dropdown'); //remove opening from this btn
+        secondInnerLinks.classList.add('inactive-dropdown'); //add closure to this btn
+    } else { //else the other btn is open
+        firstInnerLinks.classList.remove('active-dropdown'); //remove opening from other btn
+        secondInnerLinks.classList.toggle('active-dropdown'); //toggle open-close to this btn
+    }
 })
